3fb2d7b0f8db212822c1839d3fbf73ea4a16b5ad
Paul Wooten <paul.wooten@hyland.com>
2025-06-06 14:54:25 -0400

    feat: working https proxy

---
+++ https-proxy/doc-server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const customRouter = function (req) {
+    return 'http://www.example.org'; // protocol + host
+};
+
+const options = {
+    target: 'http://localhost:8000',
+    router: customRouter,
+};
+
+const myProxy = createProxyMiddleware(options);
+
+const app = express();
+app.use(myProxy); // add the proxy to express
+
+app.listen(3000);
--- https-proxy/server.ts
+++ https-proxy/server.ts
@@ -1,33 +1,29 @@
-import fs from "fs";
-import httpProxy from "http-proxy";
-import https from "https";
+import express from 'express';
+import fs from 'fs';
+import https from 'https';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const httpsOptions = {
     key: fs.readFileSync("C:\\certificates\\localhost.key"),
     cert: fs.readFileSync("C:\\certificates\\localhost.crt"),
 };
 
-const targets = {
-    fakeEmr: "https://localhost:4000",
-    opw: "http://hyl-cpql6j3",
-};
-
 const port = 3001;
-const proxyOptions = {
-    ssl: httpsOptions,
-    target: targets.fakeEmr,
-    secure: false,
-};
-const proxy = httpProxy.createProxyServer(proxyOptions);
-const server = https.createServer(httpsOptions, (req, res) => {
-    console.log("Request received:", req.url, req.headers);
-    proxy.web(req, res, { target: targets.fakeEmr }, (err) => {
-        console.error("Proxy error:", err);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Something went wrong while proxying the request.");
-    });
-});
+const app = express();
 
+const middlewareOptions = {
+    router: (req: any) => {
+        console.log(`Request URL: ${req.url}`);
+        return 'https://github.com/pawooten';
+    },
+    logger: console,
+    changeOrigin: true,
+};
+
+const proxy = createProxyMiddleware(middlewareOptions);
+app.use(proxy);
+
+const server = https.createServer(httpsOptions, app);
 server.listen(port, () => {
-    console.log(`HTTPS Proxy to ${targets.fakeEmr} listening on port ${port}`);
+    console.log(`HTTPS Server listening on port ${port}`);
 });
